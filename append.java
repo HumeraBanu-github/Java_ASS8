79984318e366830285c7eed854130d5afaa24d0a
HumeraBanu-github <93283737+HumeraBanu-github@users.noreply.github.com>
2021-11-12 16:28:38 +0530

    4,5,6,7 question

---
+++ append.java
@@ -0,0 +1,17 @@
+package Ass7.ques5;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class append {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("hello");
+        list.add("bye");
+        StringBuilder sb= new StringBuilder();
+        list.forEach(s -> sb.append(s.charAt(0)));
+        System.out.println(sb);
+    }
+}
